109889caa60f14d59ccaaae02b2706fa2280df65
Wolf Van Herreweghe <Wolf@exiin.com>
2019-10-21 02:34:53 +0200

    Various updates for testing the formation system

--- Assets/AstarPathfindingProject/ExampleScenes/ExampleScripts/MineBotAnimation.cs
+++ Assets/AstarPathfindingProject/ExampleScenes/ExampleScripts/MineBotAnimation.cs
@@ -31,14 +31,6 @@ namespace Pathfinding.Examples {
 		/// <summary>Speed relative to velocity with which to play animations</summary>
 		public float animationSpeed = 0.2F;
 
-		/// <summary>
-		/// Effect which will be instantiated when end of path is reached.
-		/// See: OnTargetReached
-		/// </summary>
-		public GameObject endOfPathEffect;
-
-		bool isAtDestination;
-
 		IAstarAI ai;
 		Transform tr;
 
@@ -53,36 +45,13 @@ namespace Pathfinding.Examples {
 			anim["forward"].layer = 10;
 
 			// Play all animations
-			anim.Play("awake");
 			anim.Play("forward");
-
-			// Setup awake animations properties
-			anim["awake"].wrapMode = WrapMode.Clamp;
-			anim["awake"].speed = 0;
-			anim["awake"].normalizedTime = 1F;
 		}
 
 		/// <summary>Point for the last spawn of <see cref="endOfPathEffect"/></summary>
 		protected Vector3 lastTarget;
 
-		/// <summary>
-		/// Called when the end of path has been reached.
-		/// An effect (<see cref="endOfPathEffect)"/> is spawned when this function is called
-		/// However, since paths are recalculated quite often, we only spawn the effect
-		/// when the current position is some distance away from the previous spawn-point
-		/// </summary>
-		void OnTargetReached () {
-			if (endOfPathEffect != null && Vector3.Distance(tr.position, lastTarget) > 1) {
-				GameObject.Instantiate(endOfPathEffect, tr.position, tr.rotation);
-				lastTarget = tr.position;
-			}
-		}
-
 		protected void Update () {
-			if (ai.reachedEndOfPath) {
-				if (!isAtDestination) OnTargetReached();
-				isAtDestination = true;
-			} else isAtDestination = false;
 
 			// Calculate the velocity relative to this transform's orientation
 			Vector3 relVelocity = tr.InverseTransformDirection(ai.velocity);
