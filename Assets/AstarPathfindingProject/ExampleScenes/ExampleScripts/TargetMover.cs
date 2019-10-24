32c507aa3f452e37973902575bc94391d0aa8f66
Wolf Van Herreweghe <wolf@exiin.com>
2019-10-24 22:13:29 +0200

    Test Components

--- Assets/AstarPathfindingProject/ExampleScenes/ExampleScripts/TargetMover.cs
+++ Assets/AstarPathfindingProject/ExampleScenes/ExampleScripts/TargetMover.cs
@@ -1,7 +1,25 @@
+using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
-namespace Pathfinding {
+namespace Pathfinding
+{
+
+	public class testTraversable : ITraversalProvider
+	{
+		public HashSet<GraphNode> blockedNodes;
+
+		public bool CanTraverse(Path path, GraphNode node)
+		{
+			return !blockedNodes.Contains(node);
+		}
+
+		public uint GetTraversalCost(Path path, GraphNode node)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+
 	/// <summary>
 	/// Moves the target in example scenes.
 	/// This is a simple script which has the sole purpose
@@ -11,7 +29,8 @@ namespace Pathfinding {
 	/// It is not meant to be pretty, but it does the job.
 	/// </summary>
 	[HelpURL("http://arongranberg.com/astar/docs/class_pathfinding_1_1_target_mover.php")]
-	public class TargetMover : MonoBehaviour {
+	public class TargetMover : MonoBehaviour
+	{
 		/// <summary>Mask for the raycast placement</summary>
 		public LayerMask mask;
 
@@ -20,57 +39,99 @@ namespace Pathfinding {
 
 		/// <summary>Determines if the target position should be updated every frame or only on double-click</summary>
 		public bool onlyOnDoubleClick;
+
 		public bool use2D;
 
 		Camera cam;
 
-		public void Start () {
+		testTraversable traversable = new testTraversable();
+
+		public void Start()
+		{
 			//Cache the Main Camera
 			cam = Camera.main;
 			// Slightly inefficient way of finding all AIs, but this is just an example script, so it doesn't matter much.
 			// FindObjectsOfType does not support interfaces unfortunately.
 			ais = FindObjectsOfType<MonoBehaviour>().OfType<IAstarAI>().ToArray();
 			useGUILayout = false;
+
+			HashSet<GraphNode> blockedNodes = new HashSet<GraphNode>();
+
+			foreach (NavGraph graph in AstarPath.active.graphs)
+			{
+				graph.GetNodes(node =>
+				{
+					if (node.Tag != 0)
+						blockedNodes.Add(node);
+				});
+			}
+
+			traversable.blockedNodes = blockedNodes;
 		}
 
-		public void OnGUI () {
-			if (onlyOnDoubleClick && cam != null && Event.current.type == EventType.MouseDown && Event.current.clickCount == 2) {
+		public void OnGUI()
+		{
+			if (onlyOnDoubleClick && cam != null && Event.current.type == EventType.MouseDown && Event.current.clickCount == 2)
+			{
 				UpdateTargetPosition();
 			}
 		}
 
 		/// <summary>Update is called once per frame</summary>
-		void Update () {
-			if (!onlyOnDoubleClick && cam != null) {
+		void Update()
+		{
+			if (!onlyOnDoubleClick && cam != null)
+			{
 				UpdateTargetPosition();
 			}
 		}
 
-		public void UpdateTargetPosition () {
+		public void UpdateTargetPosition()
+		{
 			Vector3 newPosition = Vector3.zero;
 			bool positionFound = false;
 
-			if (use2D) {
+			if (use2D)
+			{
 				newPosition = cam.ScreenToWorldPoint(Input.mousePosition);
 				newPosition.z = 0;
 				positionFound = true;
-			} else {
+			}
+			else
+			{
 				// Fire a ray through the scene at the mouse position and place the target where it hits
 				RaycastHit hit;
-				if (Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, mask)) {
+				if (Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, mask))
+				{
 					newPosition = hit.point;
 					positionFound = true;
 				}
 			}
 
-			if (positionFound && newPosition != target.position) {
-				target.position = newPosition;
+			if (positionFound && newPosition != target.position)
+			{
+				if (onlyOnDoubleClick)
+				{
+					GraphNode destinationNode = AstarPath.active.GetNearest(newPosition, NNConstraint.Default).node;
+					GraphNode playerNode = AstarPath.active.GetNearest(ais[0].position, NNConstraint.Default).node;
 
-				if (onlyOnDoubleClick) {
-					for (int i = 0; i < ais.Length; i++) {
-						if (ais[i] != null) ais[i].SearchPath();
+					bool isPathPossible = PathUtilities.IsPathPossible(destinationNode, playerNode, traversable);
+					Debug.Log(isPathPossible);
+					
+					if (isPathPossible)
+					{
+						for (int i = 0; i < ais.Length; i++)
+						{
+							if (ais[i] != null) ais[i].SearchPath();
+						}
+					}
+					else
+					{
+						return;
 					}
 				}
+				
+				target.position = newPosition;
 			}
 		}
 	}
